45fb3c1497fe66fac307531e8c18769558421967
giturtle <44741844+giturtle@users.noreply.github.com>
2019-05-11 00:29:01 +0800

    Classes and objects

--- C++_grammar/Classes_and_objects.cpp
+++ C++_grammar/Classes_and_objects.cpp
@@ -3,60 +3,63 @@
 //class类的引入
 
 struct Student{
- void SetStudentInfo(const char* name, const char* gender, int age){
- strcpy(_name, name);
- strcpy(_gender, gender);
- _age = age;
- }
+  void SetStudentInfo(const char* name, const char* gender, int age){
+    strcpy(_name, name);
+    strcpy(_gender, gender);
+    _age = age;
+  }
 
- void PrintStudentInfo(){
- cout<<_name<<" "<<_gender<<" "<<_age<<endl;
- }
+  void PrintStudentInfo(){
+    cout<<_name<<" "<<_gender<<" "<<_age<<endl;
+  }
 
- char _name[20];
- char _gender[3];
- int _age;
+  char _name[20];
+  char _gender[3];
+  int _age;
 };
+
 int main(){
- Student s;
- s.SetStudentInfo("Peter", "男", 18);
- return 0;
+  Student s;
+  s.SetStudentInfo("Peter", "男", 18);
+  return 0;
 }
 
 
 
 //类的作用域
 class Person{
-public:
- void PrintPersonInfo();
-private:
-char _name[20];
- char _gender[3];
- int _age;
+  public:
+  void PrintPersonInfo();
+  private:
+  char _name[20];
+  char _gender[3];
+  int _age;
 };
+
 // 这里需要指定PrintPersonInfo是属于Person这个类域
 void Person::PrintPersonInfo(){
- cout<<_name<<" "_gender<<" "<<_age<<endl;
+  cout<<_name<<" "_gender<<" "<<_age<<endl;
 }
 
 
 //this指针
 class Date{
-public :
- void Display (){
-cout <<_year<< "-" <<_month << "-"<< _day <<endl;
- }
+  public :
+  void Display (){
+    cout <<_year<< "-" <<_month << "-"<< _day <<endl;
+  }
 
- void SetDate(int year , int month , int day){
- _year = year;
- _month = month;
- _day = day;
- }
-private :
- int _year ; // 年
- int _month ; // 月
- int _day ; // 日
+  void SetDate(int year , int month , int day){
+    _year = year;
+    _month = month;
+    _day = day;
+  }
+  private :
+  int _year ; // 年
+  int _month ; // 月
+  int _day ; // 日
 };
+
 int main(){
  Date d1, d2;
  d1.SetDate(2018,5,1);
@@ -69,16 +72,16 @@ int main(){
 
 //案例
 class A{
-public:
- void PrintA(){
- cout<<_a<<endl;
- }
+  public:
+  void PrintA(){
+   cout<<_a<<endl;
+  }
 
- void Show(){
- cout<<"Show()"<<endl;
- }
-private:
- int _a;
+  void Show(){
+   cout<<"Show()"<<endl;
+  }
+  private:
+  int _a;
 };
 int main(){
  Date* p = NULL;
