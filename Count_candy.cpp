ceca91eaf4922b94bd0527bbc1acf1ece545cfdd
giturtle <44741844+giturtle@users.noreply.github.com>
2019-05-25 23:50:02 +0800

    Count Candy Gaming

--- Count_candy.cpp
+++ Count_candy.cpp
@@ -12,9 +12,9 @@ int main(){
   int C=(d-b)/2;
   int B1=(c-a)/2;
   int B2=(b+d)/2;
-  if(B1!=B2)
-  cout<<"No";
-  else
-  cout<<A<<" "<<B1<<" "<<C;
+  if(B1!=B2)  cout<<"No";
+  else  cout<<A<<" "<<B1<<" "<<C;
   return 0;
 }
+
+
