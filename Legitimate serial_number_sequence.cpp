7b6f07cae0738a549e934a19412db009b43d07f6
giturtle <44741844+giturtle@users.noreply.github.com>
2019-05-28 23:47:23 +0800

    Legitimate serial number sequence

---
+++ Legitimate serial_number_sequence.cpp
@@ -0,0 +1,33 @@
+
+题目ID：36939-合法括号序列判断
+链接：https://www.nowcoder.com/practice/d8acfa0619814b2d98f12c071aef20d4?tpId=8&&tqId=1103
+9&rp=1&ru=/activity/oj&qru=/ta/cracking-the-coding-interview/question-ranking
+
+
+
+
+class Parenthesis {
+public:
+bool chkParenthesis(string A, int n) {
+// write code here
+stack<char> sc;
+for (auto ele : A) {
+switch (ele) {
+case '(':
+sc.push(ele);
+break;
+case ')':
+{
+if (sc.empty() || sc.top() != '(')
+return false;
+else
+sc.pop();
+}
+break;
+default:
+return false;
+}
+}
+return true;
+}
+};
