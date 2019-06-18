0494987e443485116d44991e7b3cdf807bd9ec7e
giturtle <44741844+giturtle@users.noreply.github.com>
2019-06-18 23:56:06 +0800

    Transform HEX between two nums

--- transform_num.cpp
+++ transform_num.cpp
@@ -1,7 +1,6 @@
 
 //https://www.nowcoder.com/cts/16754304/summary?id=079D6DF3AAD70A2B#4/{%22uid%22%3A%22079D6DF3AAD70A2B%22%2C%22type%22%3A%224%22%2C%22index%22%3A1}
 
-
 #include <iostream>
 #include <math.h>
 using namespace std;
