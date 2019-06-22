76f079dfdfd7839f62b41a70840721453cabe2df
giturtle <44741844+giturtle@users.noreply.github.com>
2019-06-22 12:50:15 +0800

    Inherit （继承）

--- C++_grammar/Inherit.cpp
+++ C++_grammar/Inherit.cpp
@@ -84,3 +84,138 @@ void Test (){
    Student* ps2 = (Student*)pp; // 这种情况转换时虽然可以，但是会存在越界访问的问题
    ps2->_No = 10;
 }
+
+
+
+//继承的作用域
+// Student的_num和Person的_num构成隐藏关系，可以看出这样代码虽然能跑，但是非常容易混淆
+class Person{
+  protected :
+   string _name = "小李子"; // 姓名
+   int _num = 111; // 身份证号
+};
+class Student : public Person{
+public:
+   void Print(){
+     cout<<" 姓名:"<<_name<< endl;
+     cout<<" 身份证号:"<<Person::_num<< endl;
+     cout<<" 学号:"<<_num<<endl;
+   }
+  protected:
+   int _num = 999; // 学号
+};
+void Test(){
+   Student s1;
+   s1.Print();
+};
+
+
+//重载与隐藏
+
+// B中的fun和A中的fun不是构成重载，因为不是在同一作用域
+// B中的fun和A中的fun构成隐藏，成员函数满足函数名相同就构成隐藏。
+class A{
+public:
+   void fun(){
+    cout << "func()" << endl;
+   }
+};
+class B : public A{
+public:
+ void fun(int i){
+   A::fun();
+   cout << "func(int i)->" <<i<<endl;
+ }
+};
+void Test(){
+ B b;
+ b.fun(10);
+};
+
+
+
+
+
+//6个默认成员函数，“默认”的意思就是指我们不写，编译器会变我们自动生成一个，那么在派生类中，这几个成员函数是如何生成的呢？
+//1. 派生类的构造函数必须调用基类的构造函数初始化基类的那一部分成员。
+//    如果基类没有默认的构造函数，则必须在派生类构造函数的初始化列表阶段显示调用。
+//2. 派生类的拷贝构造函数必须调用基类的拷贝构造完成基类的拷贝初始化。
+//3. 派生类的operator=必须要调用基类的operator=完成基类的复制。
+//4. 派生类的析构函数会在被调用完成后自动调用基类的析构函数清理基类成员。因为这样才能保证派生类
+//    对象先清理派生类成员再清理基类成员的顺序。
+//5. 派生类对象初始化先调用基类构造再调派生类构造。
+//6. 派生类对象析构清理先调用派生类析构再调基类的析构
+
+//派生类默认函数
+class Person{
+  public :
+ Person(const char* name = "peter")
+ : _name(name )
+ {
+ cout<<"Person()" <<endl;
+ }
+
+ Person(const Person& p)
+ : _name(p._name)
+ {
+ cout<<"Person(const Person& p)" <<endl;
+ }
+
+ Person& operator=(const Person& p )
+ {
+ cout<<"Person operator=(const Person& p)"<< endl;
+ if (this != &p)
+ _name = p ._name;
+
+ return *this ;
+ }
+
+ ~Person()
+ {
+ cout<<"~Person()" <<endl;
+ }
+protected :
+ string _name ; // 姓名
+};
+class Student : public Person
+{
+public :
+ Student(const char* name, int num)
+ : Person(name )
+ , _num(num )
+ {
+ cout<<"Student()" <<endl;
+ }
+
+ Student(const Student& s)
+ : Person(s)
+ , _num(s ._num)
+ {
+ cout<<"Student(const Student& s)" <<endl ;
+ }
+
+ Student& operator = (const Student& s )
+ {
+ cout<<"Student& operator= (const Student& s)"<< endl;
+ if (this != &s)
+ {
+ Person::operator =(s);
+ num = s ._num;
+ }
+ return *this ;
+ }
+
+ ~Student()
+ {
+ cout<<"~Student()" <<endl;
+ }
+protected :
+ int _num ; //学号
+};
+void Test ()
+{
+ Student s1 ("jack", 18);
+ Student s2 (s1);
+ Student s3 ("rose", 17);
+ s1 = s3 ;
+}
