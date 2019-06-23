ab59a9c841e8f87a75c2e3144f414c74c4c1308f
giturtle <44741844+giturtle@users.noreply.github.com>
2019-06-23 23:05:58 +0800

    String To Num

---
+++ String_To_Num.cpp
@@ -0,0 +1,37 @@
+
+
+
+
+//https://www.nowcoder.com/practice/1277c681251b4372bdef344468e4f26etpId=13&&tqId=11202&rp=6&ru=/activity/oj&qru=/ta/coding-interviews/question-ranking
+
+
+class Solution {
+public:
+int StrToInt(string str)
+{
+if(str.empty())
+return 0;
+int symbol = 1;
+if(str[0] == '-') //处理负号
+{
+symbol = -1;
+str[0] = '0'; //这里是字符'0',不是0
+}
+else if(str[0] == '+') //处理正号
+{
+symbol = 1;
+str[0] = '0';
+}
+int sum = 0;
+for(int i=0;i<str.size();++i)
+{
+if(str[i] < '0' || str[i] > '9')
+{
+sum = 0;
+break;
+}
+sum = sum *10 + str[i] - '0';
+}
+return symbol * sum;
+}
+};
