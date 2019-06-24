3b9849993de625c640c4a1fb7f3a306c0f1648c1
giturtle <44741844+giturtle@users.noreply.github.com>
2019-06-24 12:07:37 +0800

    Inheritance

--- C++_grammar/Inheritance.cpp
+++ C++_grammar/Inheritance.cpp
@@ -32,6 +32,19 @@ int main(){
 }
 
 
+
+//1. 基类private成员在派生类中无论以什么方式继承都是不可见的。这里的不可见是指基类的私有成员还是
+//  被继承到了派生类对象中，但是语法上限制派生类对象不管在类里面还是类外面都不能去访问它。
+//2. 基类private成员在派生类中是不能被访问，如果基类成员不想在类外直接被访问，但需要在派生类中能
+//  访问，就定义为protected。可以看出保护成员限定符是因继承才出现的。
+//3. 实际上面的表格我们进行一下总结会发现，基类的私有成员在子类都是不可见。基类的其他成员在子类
+//  的访问方式 == Min(成员在基类的访问限定符，继承方式)，public > protected > private。
+//4. 使用关键字class时默认的继承方式是private，使用struct时默认的继承方式是public，不过最好显示的
+//  写出继承方式。
+//5. 在实际运用中一般使用都是public继承，几乎很少使用protetced/private继承，也不提倡使用
+//  protetced/private继承，因为protetced/private继承下来的成员都只能在派生类的类里面使用，实际中
+//  扩展维护性不强。
+
 //区别
 class Person{
   public :
@@ -88,6 +101,7 @@ void Test (){
 
 
 //继承的作用域
+
 // Student的_num和Person的_num构成隐藏关系，可以看出这样代码虽然能跑，但是非常容易混淆
 class Person{
   protected :
@@ -108,6 +122,8 @@ void Test(){
    Student s1;
    s1.Print();
 };
+
+
 
 
 //重载与隐藏
@@ -146,76 +162,140 @@ void Test(){
 //5. 派生类对象初始化先调用基类构造再调派生类构造。
 //6. 派生类对象析构清理先调用派生类析构再调基类的析构
 
-//派生类默认函数
+class Person {
+public:
+	//构造函数
+	Person(const char* name = "peter")
+		: _name(name)
+	{
+		cout << "Person()" << endl;
+	}
+
+	//拷贝构造
+	Person(const Person& p)
+		: _name(p._name)
+	{
+		cout << "Person(const Person& p)" << endl;
+	}
+
+	//赋值操作符重载函数
+	Person& operator=(const Person& p){
+		cout << "Person operator=(const Person& p)" << endl;
+		if (this != &p) {
+			_name = p._name;
+		}
+		return *this;
+	}
+
+	//析构函数
+	~Person()
+	{
+		cout << "~Person()" << endl;
+	}
+protected:
+	string _name; // 姓名
+};
+
+class Student : public Person{
+public:
+	Student(const char* name, int num)
+		: Person(name)
+		, _num(num)
+	{
+		cout << "Student()" << endl;
+	}
+
+	Student(const Student& s)
+		: Person(s)
+		, _num(s._num)
+	{
+		cout << "Student(const Student& s)" << endl;
+	}
+
+	Student& operator = (const Student& s){
+		cout << "Student& operator= (const Student& s)" << endl;
+		if (this != &s)
+		{
+			Person::operator =(s);
+			_num = s._num;
+		}
+		return *this;
+	}
+
+	~Student(){
+		cout << "~Student()" << endl;
+	}
+protected:
+	int _num; //学号
+};
+
+void Func(){
+	Student s1("jack", 18);
+	Student s2(s1);
+	Student s3("rose", 17);
+	s1 = s3;
+}
+
+
+
+//友元与继承
+class Student;	//前置声明，否则先定义的 Person 类无法识别 Student 类
+
 class Person{
-  public :
- Person(const char* name = "peter")
- : _name(name )
- {
- cout<<"Person()" <<endl;
- }
-
- Person(const Person& p)
- : _name(p._name)
- {
- cout<<"Person(const Person& p)" <<endl;
- }
-
- Person& operator=(const Person& p )
- {
- cout<<"Person operator=(const Person& p)"<< endl;
- if (this != &p)
- _name = p ._name;
-
- return *this ;
- }
-
- ~Person()
- {
- cout<<"~Person()" <<endl;
- }
-protected :
- string _name ; // 姓名
+public:
+	friend void Display(const Person& p, const Student& s);	//友元
+protected:
+	string _name; // 姓名
 };
-class Student : public Person
-{
-public :
- Student(const char* name, int num)
- : Person(name )
- , _num(num )
- {
- cout<<"Student()" <<endl;
- }
 
- Student(const Student& s)
- : Person(s)
- , _num(s ._num)
- {
- cout<<"Student(const Student& s)" <<endl ;
- }
-
- Student& operator = (const Student& s )
- {
- cout<<"Student& operator= (const Student& s)"<< endl;
- if (this != &s)
- {
- Person::operator =(s);
- num = s ._num;
- }
- return *this ;
- }
-
- ~Student()
- {
- cout<<"~Student()" <<endl;
- }
-protected :
- int _num ; //学号
+class Student : public Person{
+protected:
+	int _stuNum; // 学号
 };
-void Test ()
-{
- Student s1 ("jack", 18);
- Student s2 (s1);
- Student s3 ("rose", 17);
- s1 = s3 ;
+
+void Display(const Person& p, const Student& s){
+	cout << p._name << endl;
+	cout << s._stuNum << endl;
+}
+
+void main(){
+	Person p;
+	Student s;
+	Display(p, s);
+}
+
+
+
+
+
+
+
+//菱形继承问题
+class Person{
+public:
+	string _name; // 姓名
+};
+
+class Student : public Person{
+protected:
+	int _num; //学号
+};
+
+class Teacher : public Person{
+protected:
+	int _id; // 职工编号
+};
+
+class Assistant : public Student, public Teacher{
+protected:
+	string _majorCourse; // 主修课程
+};
+void Test(){
+	// 这样会有二义性无法明确知道访问的是哪一个
+	Assistant a;
+	a._name = "peter";
+
+	// 需要显示指定访问哪个父类的成员可以解决二义性问题，但是数据冗余问题无法解决
+	a.Student::_name = "xxx";
+	a.Teacher::_name = "yyy";
 }
