05f64e8df525f147a2a1aa6f5fde79d7de7cbabe
giturtle <44741844+giturtle@users.noreply.github.com>
2019-07-02 00:06:09 +0800

    Find Num in Two-Dimensional（二维） Array

---
+++ Finding_Two_Dimensional_Array.cpp
@@ -0,0 +1,23 @@
+
+
+
+bool Find(int *arr, int row, int col, int num) {
+	bool found = false;
+
+	if (arr != nullptr && row > 0 && col > 0) {
+		int i = 0;
+		int j = col - 1;
+		while (i < row && j >= 0) {
+			if (arr[i * col + j] == num) {
+				found = true;
+				break;
+			}
+			else if (arr[i * col + j] > num) {
+				--j;
+			}
+			else
+				++i;
+		}
+	}
+	return found;
+}
