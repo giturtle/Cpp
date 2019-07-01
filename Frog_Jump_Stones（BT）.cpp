f3feb0f0481a9f4a41a8e2e9cf6a3cd4447a3e7c
giturtle <44741844+giturtle@users.noreply.github.com>
2019-07-01 23:44:50 +0800

    Frog Jump Stones Problem

--- Frog_Jump_Stones（BT）.cpp
+++ Frog_Jump_Stones（BT）.cpp
@@ -5,6 +5,13 @@
 //一只青蛙一次可以跳上1级台阶，也可以跳上2级，和多级
 //求青蛙跳上一个n级台阶的总共有多少跳法
 
+
+
+//F(n) = F（n-1）+F(n-2)+...+F(1)
+//F(n-1) = F（n-2）+F(n-3)+...+F(1)
+
+//两个式子相减，错位相减，很容易得出 F(n) = 2F(n-1)
+
 class Solution {
 public:
     int jumpFloorII(int target) {
@@ -15,7 +22,6 @@ public:
             result = 1;
         else
             result = 2 * jumpFloorII(target - 1);
-            // 2^(n-1)
         return result;
     }
 };
