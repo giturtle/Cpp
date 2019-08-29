3bc5e380d232492dab6f40ed3a5ddd5ec96ace3e
giturtle <44741844+giturtle@users.noreply.github.com>
2019-08-29 23:56:24 +0800

    Binary Insert question

---
+++ Binary_Insert.cpp
@@ -0,0 +1,13 @@
+
+
+//[https://www.nowcoder.com/practice/30c1674ad5694b3f8f0bc2de6f005490tpId=8&&tqId=11019&rp=1&ru=/activity/oj&qru=/ta/cracking-the-coding-interview/question-ranking]
+
+class BinInsert {
+public:
+    int binInsert(int n, int m, int j, int i) {
+        m <<= j;
+        return n | m;
+    }
+};
+
+
